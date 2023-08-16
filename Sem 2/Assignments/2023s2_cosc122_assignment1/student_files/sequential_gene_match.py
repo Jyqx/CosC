212b7eea92a3ef636db84225f90552e6e97424c9
Luke <ldo59@uclive.ac.nz>
2023-08-16 19:39:13 +1200

    adsadsa

--- Sem 2/Assignments/2023s2_cosc122_assignment1/student_files/sequential_gene_match.py
+++ Sem 2/Assignments/2023s2_cosc122_assignment1/student_files/sequential_gene_match.py
@@ -1,5 +1,5 @@
 """ Filename: sequential_gene_match.py
-    Author: your name should potentially go here
+    Author: Luke Donaldson-Scott
 
 A module for finding the genetic similarity between
 two genomes using a linear search.
@@ -8,7 +8,7 @@ two genomes using a linear search.
 from classes import GeneList
 
 # Uncomment the following line to be able to make your own testing Genes
-
+from classes import Genome, Gene
 
 def sequential_gene_match(first_genome, second_genome):
     """ This function takes two Genome objects, and returns a GeneList
@@ -20,8 +20,13 @@ def sequential_gene_match(first_genome, second_genome):
     """
     common_genes = GeneList()
     comparisons = 0
-    # ---start student section---
-    pass
+    # ===start student section===
+    for gene1 in first_genome:
+        # Iterate through genes in the second genome
+        for gene2 in second_genome:
+            comparisons += 1
+            if gene1 == gene2:
+                common_genes.append(gene1)
     # ===end student section===
     return common_genes, comparisons
 
